4e2c9eaeddb605dd599886d05a162fb758f59f5a
kychenzi <kychenzi@tsmc.com.tw>
2018-12-02 22:07:47 +0800

    Initialization
    Initialization as springboot project

---
+++ src/main/java/com/kychenzi/InvestmentNotice/InvestmentNoticeApplication.java
@@ -0,0 +1,12 @@
+package com.kychenzi.InvestmentNotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentNoticeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kychenzi/InvestmentNotice/InvestmentNoticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kychenzi.InvestmentNotice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestmentNoticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
